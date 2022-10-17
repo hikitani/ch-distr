8e5ac917c3e7704cddc27ca33e2c53a755909662
hikitani <kazbekmamakaev@mail.ru>
2022-10-17 23:34:17 +0300

    test: add shard

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/google/uuid v1.3.0
 	github.com/stretchr/testify v1.8.0
 	go.uber.org/multierr v1.8.0
+	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4
 )
 
 require (
@@ -30,7 +31,6 @@ require (
 	go.uber.org/atomic v1.10.0 // indirect
 	go.uber.org/zap v1.23.0 // indirect
 	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
-	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4 // indirect
 	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
 	golang.org/x/tools v0.1.12 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
---
+++ shard_test.go
@@ -0,0 +1,132 @@
+package chdistr
+
+import (
+	"context"
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/ClickHouse/ch-go"
+	"github.com/ClickHouse/ch-go/proto"
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/sync/errgroup"
+)
+
+func TestShard(t *testing.T) {
+	type (
+		Foo struct {
+			Ts   time.Time
+			Ts6  proto.DateTime64
+			Foo  string
+			Bar  uint8
+			Long proto.UInt256
+		}
+	)
+
+	const (
+		ddlCreate = `CREATE TABLE IF NOT EXISTS default.table_insert
+	(
+		ts   DateTime,
+		ts6  DateTime64(6),
+		foo  String,
+		bar  UInt8,
+		long UInt256
+	)
+	ENGINE = MergeTree
+	ORDER BY ts`
+		ddlDrop = "DROP TABLE IF EXISTS default.table_insert"
+	)
+
+	ctx := context.Background()
+	conn, err := ch.Dial(ctx, ch.Options{
+		Address: "127.0.0.1:9000",
+	})
+	if err != nil {
+		t.Fatal("ch dial: ", err)
+	}
+	t.Cleanup(func() { conn.Close() })
+
+	err = conn.Do(ctx, ch.Query{
+		Body: ddlCreate,
+	})
+	if err != nil {
+		t.Fatal("create table: ", err)
+	}
+	t.Cleanup(func() {
+		conn.Do(ctx, ch.Query{
+			Body: ddlDrop,
+		})
+	})
+
+	sh, err := newShard[Foo](ctx, NewHostInfo("localhost"), ch.Options{
+		Address: "127.0.0.1:9000",
+	})
+	if err != nil {
+		t.Fatal("create shard: ", err)
+	}
+	t.Cleanup(func() { sh.close() })
+
+	ctx, cancel := context.WithCancel(ctx)
+	datach := make(chan Foo)
+	sharedch := make(chan *batch[Foo], 1)
+	stch := make(chan<- Node, 2)
+
+	errg, ctx := errgroup.WithContext(ctx)
+	errg.Go(func() error {
+		flushInterval := 500 * time.Millisecond
+		return sh.start(ctx, flushInterval, "table_insert", datach, sharedch, stch)
+	})
+
+	go func() {
+		for i := 0; i < 1000; i++ {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				datach <- Foo{
+					Ts:   time.Now(),
+					Ts6:  proto.ToDateTime64(time.Now(), proto.PrecisionMicro),
+					Foo:  randstr(10),
+					Bar:  uint8(rand.Uint32()),
+					Long: proto.UInt256FromUInt64(rand.Uint64()),
+				}
+			}
+		}
+	}()
+
+	time.AfterFunc(time.Second, func() {
+		cancel()
+	})
+
+	if err := errg.Wait(); err != nil {
+		t.Fatal("shard executing err: ", err)
+	}
+
+	ctx = context.Background()
+	var data proto.ColDateTime
+	err = conn.Do(ctx, ch.Query{
+		Body: "SELECT ts FROM default.table_insert",
+		Result: proto.Results{
+			{Name: "ts", Data: &data},
+		},
+		OnResult: func(ctx context.Context, block proto.Block) error {
+			t.Log(block.Rows)
+			return nil
+		},
+	})
+	if err != nil {
+		t.Fatal("select from table_insert: ", err)
+	}
+
+	assert.Equal(t, 1000, data.Rows())
+}
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func randstr(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
